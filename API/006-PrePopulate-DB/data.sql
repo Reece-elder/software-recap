725d2ab83b0e2f47356acc1f9d06b4c9bebce69e
reece-elder <reece.elder@qa.com>
2021-07-30 13:10:36 +0100

    Spring stuff

---
+++ API/006-PrePopulate-DB/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO cake` (`flavour`, `size`, `icing`) VALUES 
+('choc', 23, true ), 
+('strawberry', 13, false ), 
+('vanilla', 10, true );
---
+++ API/006-PrePopulate-DB/theory.md
@@ -0,0 +1,4 @@
+# Database Population
+
+By default Spring will look for a file called `data.sql` and populate database with the sql statements
+
